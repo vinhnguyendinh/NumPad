9c86a64148d83b5aec5be914433c365337a755eb
Nguyen Vinh <vinhnd@ominext.com>
2019-04-03 08:51:23 +0700

    Initial Commit

---
+++ PhonePadCollectionViewTests/PhonePadCollectionViewTests.swift
@@ -0,0 +1,34 @@
+//
+//  PhonePadCollectionViewTests.swift
+//  PhonePadCollectionViewTests
+//
+//  Created by Nguyen Vinh on 4/3/19.
+//  Copyright © 2019 Nguyen Vinh. All rights reserved.
+//
+
+import XCTest
+@testable import PhonePadCollectionView
+
+class PhonePadCollectionViewTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
